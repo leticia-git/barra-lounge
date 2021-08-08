e9c28c267caf393c4e6dd123ef82dfa7f644b6b5
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-08 17:45:44 -0300

    Alteração header

--- wp-content/themes/base/header.php
+++ wp-content/themes/base/header.php
@@ -20,7 +20,8 @@
 		} ?>
 	</title>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 	<?php wp_head(); ?>
 </head>
 
@@ -48,6 +49,34 @@
 							</div>
 						</div>
 					</div>
+					<div class="banner">
+						<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+							<div class="carousel-indicators">
+								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+							</div>
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<img src="..." class="d-block w-100" alt="...">
+								</div>
+								<div class="carousel-item">
+									<img src="..." class="d-block w-100" alt="...">
+								</div>
+								<div class="carousel-item">
+									<img src="..." class="d-block w-100" alt="...">
+								</div>
+							</div>
+							<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="visually-hidden">Previous</span>
+							</button>
+							<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="visually-hidden">Next</span>
+							</button>
+						</div>
+					</div>
 					<div class="texto-header">
 						<h1>
 							<span class="span">Vivencie</span>
@@ -55,17 +84,7 @@
 							<span class="span">em um único lugar</span>
 						</h1>
 					</div>
-					<div class="ambientes">
-						<div class="foto1">
-						<a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/hexagono.png" alt=""></a>	
-						</div>
-						<div class="foto2">
-						<a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/hexagono.png" alt=""></a>	
-						</div>
-						<div class="foto3">
-							<a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/hexagono.png" alt=""></a>
-						</div> 
-					</div>
+
 				</div>
 			</div>
 		</section>
