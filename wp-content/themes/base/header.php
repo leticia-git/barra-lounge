d66a06e38963e0260131108c8e604de088022e55
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-08 20:27:14 -0300

    Alterando header

--- wp-content/themes/base/header.php
+++ wp-content/themes/base/header.php
@@ -20,6 +20,7 @@
 		} ?>
 	</title>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
 	<!-- CSS only -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 	<?php wp_head(); ?>
