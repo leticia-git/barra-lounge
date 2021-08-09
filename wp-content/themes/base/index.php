b4778b2aa8447ae1ff7ed3459f5d0101e1dc20c0
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-08 22:14:48 -0300

    Update index.php

--- wp-content/themes/base/index.php
+++ wp-content/themes/base/index.php
@@ -1,8 +1,11 @@
   
 <?php get_header(); ?>
 <main class="main">
-<div class="banner">
-						<!-- <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="section-dois">
+        <div class="container">
+            <div class="row">
+            <div class="banner">
+						<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
 							<div class="carousel-indicators">
 								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
 								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -28,6 +31,10 @@
 								<span class="visually-hidden">Next</span>
 							</button>
 						</div>
-					</div> -->
+					</div>
+            </div>
+        </div>
+    </div>
+
 </main>
 <?php get_footer(); ?>
