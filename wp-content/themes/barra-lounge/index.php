19195b53c34eed1817dc84a07280f4646aa66a59
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-26 21:50:01 -0300

    Update index.php

--- wp-content/themes/barra-lounge/index.php
+++ wp-content/themes/barra-lounge/index.php
@@ -1,63 +1,63 @@
-  
-<?php get_header(); ?>
-<main class="main">
-    <div class="section-dois">
-        <div class="container">
-            <div class="row">
-                <h2 class="titulo-casa">A CASA</h2>
-                <hr class="hr">
-				<div class="col-sm-6">
-<div class="texto-casa">
-<span>Barra Lounge, nasce com a proposta de oferecer ao </span> 
-<span>público <span class="amarelo">3 ambientes atrativos</span> em um único lugar, </span> 
-<span>inspirado no futebol a casa conta com uma super </span> 
-<span>estrutura lembrando a temática.</span> 
+  <?php get_header(); ?>
+  <main class="main">
 
-<span>Seus espaços são divididos por <span class="amarelo"> Piso Central,</span> </span> 
-<span><span class="amarelo">Mezanino</span>  e nosso incrível<span class="amarelo"> Rooftop,</span> todos contam </span> 
-<span>com espaços reservados, mesas e bistrô.</span>  
+  	<section class="section-dois">
+  		<div class="container">
+  			<div class="row">
+  				<h2 class="titulo-casa">A CASA</h2>
+  				<hr class="hr">
+  				<div class="col-sm-6">
+  					<div class="texto-casa">
+  						<span>Barra Lounge, nasce com a proposta de oferecer ao </span>
+  						<span>público <span class="amarelo">3 ambientes atrativos</span> em um único lugar, </span>
+  						<span>inspirado no futebol a casa conta com uma super </span>
+  						<span>estrutura lembrando a temática.</span>
 
-<span>O trabalho de nosso time te faz desfrutar de um</span>  
-<span>excelente atendimento,  gastronomia típica brasileira, </span> 
-<span>drinks de alta qualidade, tabacaria, </span> 
-<span>proporcionado muita diversão, música </span> 
-<span>e entretenimento para você!</span> 
-</div>
-</div>
-</div>
-</div>
-<div class="col-sm-6">
-<div class="banner">
-						<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-							<div class="carousel-indicators">
-								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-							</div>
-							<div class="carousel-inner">
-								<div class="carousel-item active">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_2.jpg" class="d-block w-100" alt="...">
-								</div>
-								<div class="carousel-item">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_3.jpg" class="d-block w-100" alt="...">
-								</div>
-								<div class="carousel-item">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_4.jpg" class="d-block w-100" alt="...">
-								</div>
-							</div>
-							<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-								<span class="visually-hidden">Previous</span>
-							</button>
-							<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-								<span class="carousel-control-next-icon" aria-hidden="true"></span>
-								<span class="visually-hidden">Next</span>
-							</button>
-						</div>
-					</div>
-            </div>
-</div>
+  						<span>Seus espaços são divididos por <span class="amarelo"> Piso Central,</span> </span>
+  						<span><span class="amarelo">Mezanino</span> e nosso incrível<span class="amarelo"> Rooftop,</span> todos contam </span>
+  						<span>com espaços reservados, mesas e bistrô.</span>
+
+  						<span>O trabalho de nosso time te faz desfrutar de um</span>
+  						<span>excelente atendimento, gastronomia típica brasileira, </span>
+  						<span>drinks de alta qualidade, tabacaria, </span>
+  						<span>proporcionado muita diversão, música </span>
+  						<span>e entretenimento para você!</span>
+  					</div>
+  				</div>
+  				<div class="col-sm-6">
+					  <div class="banner">
+					  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  					<div class="carousel-indicators">
+  						<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+  						<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  						<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  					</div>
+  					<div class="carousel-inner">
+  						<div class="carousel-item active">
+  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_2.jpg" class="d-block w-100" alt="...">
+  						</div>
+  						<div class="carousel-item">
+  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_3.jpg" class="d-block w-100" alt="...">
+  						</div>
+  						<div class="carousel-item">
+  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_4.jpg" class="d-block w-100" alt="...">
+  						</div>
+  					</div>
+  					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  						<span class="visually-hidden">Previous</span>
+  					</button>
+  					<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+  						<span class="visually-hidden">Next</span>
+  					</button>
+  				</div>
+			</div>
+			  </div>
+  			</div>
+  		</div>
+  	</section>
 
 
-</main>
-<?php get_footer(); ?>
+  </main>
+  <?php get_footer(); ?>
