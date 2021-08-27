f403e74f4c3c91aa4cd7254f8fc4ac0fee8c838d
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-27 20:49:09 -0300

    Update index.php

--- wp-content/themes/barra-lounge/index.php
+++ wp-content/themes/barra-lounge/index.php
@@ -5,57 +5,58 @@
   		<div class="container">
   			<div class="row">
   				<h2 class="titulo-casa">A CASA</h2>
-  				<hr class="hr">
-  				<div class="col-sm-6">
-  					<div class="banner">
-  						<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  							<div class="carousel-indicators">
-  								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-  								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-  								<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  							</div>
-  							<div class="carousel-inner">
-  								<div class="carousel-item active">
-  									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_2.jpg" class="d-block w-50" alt="...">
-  								</div>
-  								<div class="carousel-item">
-  									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_3.jpg" class="d-block w-50" alt="...">
-  								</div>
-  								<div class="carousel-item">
-  									<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_4.jpg" class="d-block w-50" alt="...">
-  								</div>
-  							</div>
-  							<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-  								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-  								<span class="visually-hidden">Previous</span>
-  							</button>
-  							<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-  								<span class="carousel-control-next-icon" aria-hidden="true"></span>
-  								<span class="visually-hidden">Next</span>
-  							</button>
+				  <hr class="hr">
+				  <div class="subtexto-casa">
+					<p class="texto"><span class="amarelo">Barra Lounge, nasce com a proposta de oferecer ao 
+  					público 3 ambientes atrativos em um único lugar</span>
+  					inspirado no futebol a casa conta com uma super 
+  					estrutura lembrando a temática.</p>
+				  </div>
+				  <div class="banner">
+					  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  					<div class="carousel-indicators">
+  						<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+  						<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+  						<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  					</div>
+  					<div class="carousel-inner">
+  						<div class="carousel-item active">
+  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_2.jpg" class="d-block w-100" alt="...">
+  						</div>
+  						<div class="carousel-item">
+  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_3.jpg" class="d-block w-100" alt="...">
+  						</div>
+  						<div class="carousel-item">
+  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/Screenshot_4.jpg" class="d-block w-100" alt="...">
   						</div>
   					</div>
-  					<div class="col-sm-6">
-  						<div class="texto-casa">
+  					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  						<span class="visually-hidden">Previous</span>
+  					</button>
+  					<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+  						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+  						<span class="visually-hidden">Next</span>
+  					</button>		
+					  </div>
+					  
+  					<!-- <div class="texto-casa">
 
 
-  							<span class="texto">Seus espaços são divididos por <span class="amarelo"> Piso Central, </span> </span>
-  							<span class="texto"><span class="amarelo"> Mezanino</span> e nosso incrível <span class="amarelo">Rooftop,</span> todos contam </span>
-  							<span class="texto">com espaços reservados, mesas e bistrô.</span>
+  						<span class="texto">Seus espaços são divididos por <span class="amarelo"> Piso Central, 	</span> </span>
+  						<span class="texto"><span class="amarelo"> Mezanino</span> e nosso incrível <span class="amarelo">Rooftop,</span> todos contam </span>
+  						<span class="texto">com espaços reservados, mesas e bistrô.</span>
 
-  							<span class="texto">O trabalho de nosso time te faz desfrutar de um</span>
-  							<span class="texto">excelente atendimento, gastronomia típica brasileira, </span>
-  							<span class="texto">drinks de alta qualidade, tabacaria, </span>
-  							<span class="texto">proporcionado muita diversão, música </span>
-  							<span class="texto">e entretenimento para você!</span>
-  						</div>
-  					</div>
+  						<span class="texto">O trabalho de nosso time te faz desfrutar de um</span>
+  						<span class="texto">excelente atendimento, gastronomia típica brasileira, </span>
+  						<span class="texto">drinks de alta qualidade, tabacaria, </span>
+  						<span class="texto">proporcionado muita diversão, música </span>
+  						<span class="texto">e entretenimento para você!</span>
+  					</div> -->
 
-
-
-  				</div>
+			</div>
+			  </div>
   			</div>
-  		</div>
   		</div>
   	</section>
 
