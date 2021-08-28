9b3090ccfe0db6341b07be65290b38adb951574d
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-28 14:34:47 -0300

    alterando imgs

--- wp-content/themes/barra-lounge/customs/style.php
+++ wp-content/themes/barra-lounge/customs/style.php
@@ -1,7 +1,7 @@
 <?php
 function theme_enqueue_style()
 {
-  $versionStyle = '0.0.7';
+  $versionStyle = '0.0.8';
   
   wp_enqueue_style('style', get_template_directory_uri() . '/assets/dist/style.min.css', array(), $versionStyle, 'all');
 }
