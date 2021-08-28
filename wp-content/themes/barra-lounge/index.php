83d4eddc7232684cae8d711758308c1e7e0256fc
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-28 14:37:47 -0300

    Update index.php

--- wp-content/themes/barra-lounge/index.php
+++ wp-content/themes/barra-lounge/index.php
@@ -15,13 +15,13 @@
   					</div>
   					<div class="carousel-inner">
   						<div class="carousel-item active">
-  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto1.jpg" class="d-block w-100" alt="...">
+  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto1.png" class="d-block w-100" alt="...">
   						</div>
   						<div class="carousel-item">
-  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto2.jpg" class="d-block w-100" alt="...">
+  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto2.png" class="d-block w-100" alt="...">
   						</div>
   						<div class="carousel-item">
-  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto3.jpg" class="d-block w-100" alt="...">
+  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto3.png" class="d-block w-100" alt="...">
   						</div>
   					</div>
   					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
