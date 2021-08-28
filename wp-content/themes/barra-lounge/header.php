58151d413a4193dc01af81634c9b7309a8c89330
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-28 17:00:40 -0300

    .

--- wp-content/themes/barra-lounge/header.php
+++ wp-content/themes/barra-lounge/header.php
@@ -23,6 +23,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
 	<!-- CSS only -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<?php wp_head(); ?>
 </head>
 
