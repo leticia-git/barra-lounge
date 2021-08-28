5dddf32bec28d798188d3b56e6e6cd052b54e56a
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-28 15:45:13 -0300

    Subindo css

--- wp-content/themes/barra-lounge/customs/bundle.php
+++ wp-content/themes/barra-lounge/customs/bundle.php
@@ -1,8 +1,8 @@
 <?php
 function theme_enqueue_script()
 {
-  $versionBundle = '0.0.6';
-  $versionStyleJs = '0.0.6';
+  $versionBundle = '0.0.7';
+  $versionStyleJs = '0.0.7';
 
   wp_deregister_script('jquery');
   wp_enqueue_script('bundle-js', get_template_directory_uri() . '/assets/dist/bundle.min.js', array(), $versionBundle, true);
--- wp-content/themes/barra-lounge/customs/style.php
+++ wp-content/themes/barra-lounge/customs/style.php
@@ -1,7 +1,7 @@
 <?php
 function theme_enqueue_style()
 {
-  $versionStyle = '0.0.8';
+  $versionStyle = '0.0.9';
   
   wp_enqueue_style('style', get_template_directory_uri() . '/assets/dist/style.min.css', array(), $versionStyle, 'all');
 }
