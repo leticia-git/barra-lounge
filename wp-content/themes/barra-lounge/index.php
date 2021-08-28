7026cb0c5c631a5bfa7547b150a9b6463bc0b279
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-27 21:36:59 -0300

    Update index.php

--- wp-content/themes/barra-lounge/index.php
+++ wp-content/themes/barra-lounge/index.php
@@ -1,40 +1,52 @@
   <?php get_header(); ?>
   <main class="main">
   	<section class="section-dois">
-  		<div class="container">
-  			<div class="row">
-  				<h2 class="titulo-casa">A CASA</h2>
+  		<div class="container">  				
+			  <h2 class="titulo-casa">A CASA</h2>
   				<hr class="hr">
+  			<div class="row">
+
   				<!-- carrosel -->
-  				<div class="banner">
-  					<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  						<div class="carousel-indicators">
-  							<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-  							<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-  							<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  						</div>
-  					</div>
-  					<div class="carousel-inner">
-  						<div class="carousel-item active">
-  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto1.jpg" class="d-block w-100" alt="...">
-  						</div>
-  						<div class="carousel-item">
-  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto2.jpg" class="d-block w-100" alt="...">
-  						</div>
-  						<div class="carousel-item">
-  							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto3.jpg" class="d-block w-100" alt="...">
-  						</div>
-  					</div>
-  				</div>
-  				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-  					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-  					<span class="visually-hidden">Previous</span>
-  				</button>
-  				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-  					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-  					<span class="visually-hidden">Next</span>
-  				</button>
-  			<!-- //carrosel -->
+				  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto1.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto2.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/foto3.png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+  				<!-- //carrosel -->
   			<div class="col-sm-6">
   				<div class="img-casa">
   					<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/barra4.png" alt="">
