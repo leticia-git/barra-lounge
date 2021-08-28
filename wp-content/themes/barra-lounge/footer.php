cc2668a670035ceba3a1bf5f9cc5a93554f4c02b
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-28 16:39:44 -0300

    subindo footer

--- wp-content/themes/barra-lounge/footer.php
+++ wp-content/themes/barra-lounge/footer.php
@@ -1,4 +1,65 @@
 <footer class="footer">
+<section class="sec-footer">
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="siga">
+               <p>Acompanhe nossas redes</p> 
+                <div class="redes-casa">
+                    <div class="wpp">
+                    <a href="http://api.whatsapp.com/send?1=pt_BR&phone=" target="_blank" class="dg-wpp"><i class="fab fa-whatsapp" style="position:fixed;bottom:20px;right:30px; z-index: 1;"></i>
+                    <a href="">11 96049-2085</a> 
+                    </div>
+                    <div class="insta">
+                    <a href=""><i class="fab fa-instagram" style="position:fixed;bottom:20px;right:30px; z-index: 1;"></i></a>
+                    <a href="">/BarraLounge</a>
+                    </div>
+                    <div class="facebook">
+                    <a href=""><i class="fab fa-facebook" style="position:fixed;bottom:20px;right:30px; z-index: 1;"></i></a>
+                    <a href="">/BarraLounge</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="adega">
+           <p>Siga a Adega</p> 
+            </div>
+            <div class="wpp-adega">
+            <a href="http://api.whatsapp.com/send?1=pt_BR&phone=" target="_blank" class="dg-wpp"><i class="fab fa-whatsapp" style="position:fixed;bottom:20px;right:30px; z-index: 1;"></i>
+            <a href="">11 96049-2085</a>  
+            </div>
+            <div class="insta-adega">
+            <a href=""><i class="fab fa-instagram" style="position:fixed;bottom:20px;right:30px; z-index: 1;"></i></a>
+            <a href="">/AdegaBarraLounge</a>
+            </div>
+            <div class="face-adega">
+            <a href=""><i class="fab fa-facebook" style="position:fixed;bottom:20px;right:30px; z-index: 1;"></i></a>
+            <a href="">/AdegaBarraLounge</a>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="app-delivery">
+                <p>
+                    Peça já!
+                </p>
+                <div class="rappi">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/rappi.png" alt="">
+                </div>
+                <div class="ifood">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/ifood.png" alt="">
+                </div>
+                <div class="ubereats">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/ubereats.png" alt="">
+                </div>
+                <div class="zedelivery">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/images/ze.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
