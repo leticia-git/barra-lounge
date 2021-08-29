1e23aefa8daae743dea19e2b15d8874b9823ee22
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2021-08-29 15:39:15 -0300

    menu responsivo

--- wp-content/themes/barra-lounge/customs/style.php
+++ wp-content/themes/barra-lounge/customs/style.php
@@ -1,7 +1,7 @@
 <?php
 function theme_enqueue_style()
 {
-  $versionStyle = '0.2.4';
+  $versionStyle = '0.2.5';
   
   wp_enqueue_style('style', get_template_directory_uri() . '/assets/dist/style.min.css', array(), $versionStyle, 'all');
 }
